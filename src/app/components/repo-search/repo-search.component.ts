d9b2d86da54d9d908590126b7b220383c271854e
Clemo97 <lumumbaclement@gmail.com>
2022-04-19 18:58:56 +0300

    repo search html and css plus component class

--- src/app/components/repo-search/repo-search.component.ts
+++ src/app/components/repo-search/repo-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from 'src/app/class/Repo/repo';
+import { RepoNameInput } from 'src/app/class/RepoNameInput/repo-name-input';
+import { ReposTotalCount } from 'src/app/class/ReposTotalCount/repos-total-count';
 
 @Component({
   selector: 'app-repo-search',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-search.component.css']
 })
 export class RepoSearchComponent implements OnInit {
+  faSearch = faSearch;
 
-  constructor() { }
+  searchedRepo = new RepoNameInput();
+  searchedRepo1 = new RepoNameInput();
+  repo: Repo = new Repo();
+  repos: Repo[] = [];
+  totalCount: ReposTotalCount = new ReposTotalCount();
 
-  ngOnInit(): void {
+  submitRepoName() {
+    this.searchedRepo1 = this.searchedRepo;
+    this.repoService.searchRepo(this.searchedRepo1.repoName);
+    this.repos = this.repoService.repos;
+    this.totalCount = this.repoService.totalRepos;
+    this.searchedRepo = new RepoNameInput();
   }
+
+  constructor(private repoService: SearchRepoService) { }
+
+
+  ngOnInit() {
+  }
+
 
 }
