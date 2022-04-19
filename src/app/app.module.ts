3207952879a21503e5ab3b5f9539bc7886dd17de
Clemo97 <lumumbaclement@gmail.com>
2022-04-19 15:57:01 +0300

    created all 5 necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitLibComponent } from './components/git-lib/git-lib.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RepoSearchComponent } from './components/repo-search/repo-search.component';
+import { UserSearchComponent } from './components/user-search/user-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitLibComponent,
+    LandingPageComponent,
+    NotFoundComponent,
+    RepoSearchComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule,
