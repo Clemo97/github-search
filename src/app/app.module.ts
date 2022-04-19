e141ed872528136e3a0b6585c0c7c3923ed2a034
Clemo97 <lumumbaclement@gmail.com>
2022-04-19 21:40:21 +0300

    add imports to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitLibComponent } from './components/git-lib/git-lib.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { RepoSearchComponent } from './components/repo-search/repo-search.component';
 import { UserSearchComponent } from './components/user-search/user-search.component';
-import { RepoHighlightDirective } from './directives/RepoHighlight/repo-highlight.directive';
+import { RepoSearchComponent } from './components/repo-search/repo-search.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { DatePipePipe } from './pipes/date-pipe/date-pipe.pipe';
+import { RepoHighlightDirective } from './directives/RepoHighlight/repo-highlight.directive';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitLibComponent,
-    LandingPageComponent,
-    NotFoundComponent,
-    RepoSearchComponent,
     UserSearchComponent,
+    RepoSearchComponent,
+    DatePipePipe,
     RepoHighlightDirective,
-    DatePipePipe
+    NotFoundComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
